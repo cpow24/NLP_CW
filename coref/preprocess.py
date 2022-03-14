8dd20d7eb98c4589ab884f7eef3f0ef40d6face9
Luke Hollingsworth <luke.hollingsworth.21@ucl.ac.uk>
2022-03-14 11:42:25 +0000

    all files processed to remove non-person entities and <undefined> characters

--- coref/preprocess.py
+++ coref/preprocess.py
@@ -2,18 +2,30 @@ import numpy as np
 import pandas as pd
 import os
 
-def PullFilenames(folder, filetype):
-    folder = os.fsencode(folder)
+def PullFilenames(path, filetype):
+    folder = os.fsencode(path)
     filenames = []
 
     # Create list of filenames of interest
     for file in os.listdir(folder):
         filename = os.fsdecode(file)
 
-        if filename.endswith('.ann'):
+        if filename.endswith(filetype):
             filenames.append(filename)
     
     return filenames
+
+# def RemoveUndefined(path, filenames):
+#     for filename in filenames:
+#         print("Processing: ", filename)
+#         location = '%s/%s' % (path, filename)
+#         with open(location) as file:
+#             for line in file:
+#                 if '“' in line:
+#                     processed_line = line.replace('“', '"')
+#                 elif '”' in line:
+#                     processed_line = line.replace('”', '"')
+#     return
 
 def ProcessFiles(path, filetype):
     filenames = PullFilenames(path, filetype)
@@ -26,16 +38,24 @@ def ProcessFiles(path, filetype):
         print(processed_file_location)
         with open(location) as file:
             for line in file:
-                if 'PER' not in line:
-                    continue
+                if filetype == '.conll':
+                    if '(' not in line or ')' not in line:
+                        continue
+                    else:
+                        processed_file.append(line)
                 else:
-                    processed_file.append(line)
+                    if 'PER' not in line:
+                        continue
+                    else:
+                        processed_file.append(line)
 
         with open(processed_file_location, 'x') as f:
             for line in processed_file:
                 f.write(line)
 
     return
+
+# -------------------------------------------------------- #
     
-path = 'unprocessed_files/brat'
-ProcessFiles(path=path, filetype='.ann')
+path = 'unprocessed_files/conll'
+ProcessFiles(path=path, filetype='.conll')
